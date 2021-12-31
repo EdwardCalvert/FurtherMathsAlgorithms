25fe299544a06bc57b12a2235992e40df7137ae4
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2021-12-31 12:07:21 +0000

    Created an edge distance link

--- FurtherMathsAlgorithms/Models/DiagramLink.cs
+++ FurtherMathsAlgorithms/Models/DiagramLink.cs
@@ -4,13 +4,12 @@ namespace FurtherMathsAlgorithms.Models
 {
   public sealed class DiagramLink : LinkModel
   {
-    public DiagramLink(string name, NodeModel sourceNode, NodeModel? targetNode) :
-      base(name, sourceNode, targetNode)
+    public DiagramLink(PortModel sourcePort, PortModel? targetPort = null) :
+      base(sourcePort, targetPort)
     {
-      Name = name;
       Labels.Add(new DiagramLinkLabel(this, Name));
     }
 
-    public string Name { get; set; }
+        public string Name { get; set; }
   }
 }
