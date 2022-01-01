54fed8c09b4bdcce57de24737fe18857e31f6775
Ed Calvert <72658447+EdwardCalvert@users.noreply.github.com>
2022-01-01 17:29:55 +0000

    Minor cosmetic enhancements

--- FurtherMathsAlgorithms/Models/DirectedLink.cs
+++ FurtherMathsAlgorithms/Models/DirectedLink.cs
@@ -11,7 +11,6 @@ namespace FurtherMathsAlgorithms.Models
       Labels.Add(new DiagramLinkLabel(this, Name));
         TargetMarker = LinkMarker.NewArrow(20, 10);
     }
-
         public string Name { get; set; }
   }
 }
